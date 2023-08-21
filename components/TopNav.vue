ab7374176da6fd80b24a6de068f686dea6c2bf54
Алексей Бельченков <u608110@gmail.com>
2023-08-21 21:42:08 +0400

    prepare layouts

---
+++ components/TopNav.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { useRoute } from 'nuxt/app';
+
+  const route = useRoute();
+</script>
+
+<template>
+  <div
+    id="topNav"
+    class="fixed bg-white z-30 flex items-center w-full border-b h-[61px]"
+  >
+    <div
+        class="flex items-center justify-between w-full px-6 mx-auto"
+        :class="route.fullPath === '/' ? 'max-w-[1150px]' : ''"
+    >
+      <div :class="route.fullPath === '/' ? 'w-[80%]' : ''"></div>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
